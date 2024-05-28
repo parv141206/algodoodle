871e7957d704f2b1d96705fd906bfbd5832e5de2
parv141206 <parv141206@gmail.com>
2024-05-28 06:40:21 +0000

    Debug #1

--- app/api/code/[algorithm]/route.ts
+++ app/api/code/[algorithm]/route.ts
@@ -40,9 +40,6 @@ export async function GET(req: Request) {
       return NextResponse.json({ error: "File not found" }, { status: 404 });
     }
   } catch (error) {
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 },
-    );
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
