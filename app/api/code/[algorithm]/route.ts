c0c0b105826d4d13b77fad79e553f1e71a12f806
parv141206 <parv141206@gmail.com>
2024-05-28 07:12:11 +0000

    Debug #6

--- app/api/code/[algorithm]/route.ts
+++ app/api/code/[algorithm]/route.ts
@@ -1,26 +1,5 @@
 import { NextResponse } from "next/server";
-import { promises as fs } from "fs";
 import path from "path";
-
-const getCodeFile = async (
-  dirPath: string,
-  filename: string,
-): Promise<string | null> => {
-  const entries = await fs.readdir(dirPath, { withFileTypes: true });
-
-  for (const entry of entries) {
-    const fullPath = path.join(dirPath, entry.name);
-
-    if (entry.isDirectory()) {
-      const code = await getCodeFile(fullPath, filename);
-      if (code) return code;
-    } else if (entry.isFile() && entry.name === filename) {
-      return await fs.readFile(fullPath, "utf8");
-    }
-  }
-
-  return null;
-};
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
@@ -30,15 +9,11 @@ export async function GET(req: Request) {
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   }
 
-  const filePath = path.join("public/code/"); // Adjusted to match your project structure
+  const filePath = path.join("/code", `${algorithm}.c`); // Adjusted to match your project structure
   console.log(filePath);
   try {
-    const code = await getCodeFile(filePath, `${algorithm}.c`);
-    if (code) {
-      return NextResponse.json({ code }, { status: 200 });
-    } else {
-      return NextResponse.json({ error: "File not found" }, { status: 404 });
-    }
+    // Return the URL to the client
+    return NextResponse.redirect(filePath, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
