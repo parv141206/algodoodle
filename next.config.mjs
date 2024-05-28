3038da578aa0f41951ad69c09af9cbbb7bb54b27
parv141206 <parv141206@gmail.com>
2024-05-28 09:22:41 +0000

    try 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  files: ["public/images/code/**/*.c"],
+  files: ["public/images/code/**/*.c", "app/api/code/**/*.c"],
 };
 
 export default nextConfig;
